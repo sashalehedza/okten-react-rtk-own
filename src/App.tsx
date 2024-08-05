528ce7221e9173c3ed4cb2128967ef7bc3659c62
sashalehedza <sashalehedza@gmail.com>
2024-08-05 08:48:46 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import React, {useEffect} from 'react';
+import './App.css';
+import {useSelector} from "react-redux";
+import {useAppDispatch, useAppSelector } from "./redux/store";
+import {userActions} from "./redux/slices/userSlice";
+
+const App = () => {
+
+    let {userSlice: {users,isLoaded}} = useAppSelector(state => state);
+
+    let dispatch = useAppDispatch();
+
+    useEffect(() => {
+        dispatch(userActions.loadUsers());
+    }, []);
+
+
+    return (
+        <div>
+            {!isLoaded && <div>Loading in process....</div>}
+
+            {users.map(user => <div>{user.name}</div>)}
+        </div>
+    );
+}
+
+export default App;
